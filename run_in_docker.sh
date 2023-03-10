67a283bb5d672b387c5614dd2df38b5f61b16027
mishagrol <grol81@mail.ru>
2023-03-10 19:20:06 +0300

    add: notebooks ForestMapping

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,2 +1,2 @@
-docker run --rm -p 16433:8888 -v $PWD:/home/ -v /mnt/bulky2/mgasanov:/mnt/bulky2/mgasanov mmann1123/gw_pygis jupyter notebook --no-browser\
-     --NotebookApp.token=SecretToken --port 8888 --ip 0.0.0.0 --allow-root
+docker run --rm -p 8890:8890 -v $PWD:/home/ mmann1123/gw_pygis jupyter notebook --no-browser\
+     --NotebookApp.token=SecretToken --port 8890 --ip 0.0.0.0 --allow-root
