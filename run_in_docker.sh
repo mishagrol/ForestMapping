3b4882f15cb2df6ea3bd7522c4240d7956d8bc65
mishagrol <mgasanov@oseledets-cdise-srv1>
2024-04-15 12:32:26 +0300

    fix: remote server port

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,2 +1,2 @@
-docker run --rm -p 8891:8891 -v $PWD:/home/ mmann1123/gw_pygis jupyter notebook --no-browser\
-     --NotebookApp.token=SecretToken --port 8891 --ip 0.0.0.0 --allow-root
+docker run --rm -p 16555:16555 -v $PWD:/home/ mmann1123/gw_pygis jupyter notebook --no-browser\
+     --NotebookApp.token=SecretToken --port 16555 --ip 0.0.0.0 --allow-root
